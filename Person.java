0f811efb64558ba7995ada0c8b1f2c20aa60d0f7
Roman Adamski <fakeemail@gmail.com>
2023-05-17 22:58:18 +0200

    Person.java 2

--- Person.java
+++ Person.java
@@ -1,5 +1,9 @@
 public class Person {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
     }
 }
