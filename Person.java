3905bb9e95e504ebdb29733f0ece22685b25a691
Roman Adamski <fakeemail@gmail.com>
2023-05-18 14:54:21 +0200

    Commit comment: Adding some comment

--- Person.java
+++ Person.java
@@ -1,5 +1,12 @@
 public class Person {
     public static void main(String[] args) {
+        
+        //comment
+        //comment
+        //comment
+        //comment
+        //comment
+        //comment
         System.out.println("Hello");
         System.out.println("Hello");
         System.out.println("Hello");
