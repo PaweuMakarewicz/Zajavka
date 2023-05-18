cc6b80acb255be7d0495aed75b8fff2fb5a8ab55
Roman Adamski <fakeemail@gmail.com>
2023-05-18 14:50:45 +0200

    Commit 44: Adding some sout2

--- Person.java
+++ Person.java
@@ -5,9 +5,7 @@ public class Person {
         System.out.println("Hello");
         System.out.println("Hello");
         System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
+       
         
     }
 }
