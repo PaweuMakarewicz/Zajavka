e0fbb4996c7152ac6dc81472a7ffddd2537f570a
Roman Adamski <fakeemail@gmail.com>
2023-05-23 23:09:52 +0200

    commit: hello Stefan

--- Man.java
+++ Man.java
@@ -1,24 +1,7 @@
-<<<<<<< HEAD
-import java.math.BigDecimal;
-=======
-import sun.security.mscapi.CPublicKey;
->>>>>>> 64ecd26 (feature-commit 1: Adding variables in my-feature)
-
 public class Man {
-
-public static void main(Strings [] args) {
-
- public static final String HEIGHT = "100";
- private String name;
- private String surname;
- private BigDecimal salary;
- private String pesel;
-
- public Integer age(){
-  return 40;
+ public static void main(String[] args) {
+  System.out.println("Hello, I'm Stefan");
  }
- public String whatIsYourHobby(){
-  return "pizza";
- }
- }
+
+
 }
